85302e597f2a0bc060591b960a34f45b831b0314
luckuslin <lukasl18@nycstudents.net>
2026-02-11 09:25:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
-count = 1
+import random
 
-while count <= 5:
-    print("This is loop number", count)
-    count = count + 1
+def NumberGuess():
+    randomnumber=random.randint(1, 10)
+    while randomnumber != inputs:
+        inputs=int(input("Guess the Number:"))
+    if randomnumber != inputs:
+        print("Wrong")
+    return
+elif randomnumber == inputs:
+
+    
