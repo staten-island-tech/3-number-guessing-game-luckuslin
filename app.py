2a70978f84fbb4c761bef50337b5e0d396eeedc6
luckuslin <lukasl18@nycstudents.net>
2026-02-12 09:22:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
 import random
 
+randomnumber=random.randint(1, 10)
 def NumberGuess():
-    randomnumber=random.randint(1, 10)
-    while randomnumber != inputs:
-        inputs=int(input("Guess the Number:"))
-    if randomnumber != inputs:
-        print("Wrong")
-    return
-elif randomnumber == inputs:
+      while True:
+        Guess=int(input("What is the number?"))
+        if Guess == range(1,10):
+              return Guess
+        else : 
+            print ("Wrong Range")
 
-    
+def Check(randomnumber,Guess):
+        if Guess == randomnumber:
+             return "Correct"
+        elif Guess > randomnumber:
+             return "Too High"
+        else :
+             return "Too Low"
+
+def History():
+     Guesshistory=Check
+     Guesshistory=0
+     while Guesshistory < 10:
+        Guesshistory + 1
+     
+History ()
